7677e5d0073415280102d2769ac8cf6dea85e50e
Vlad Paplinsky <vladosmros@outlook.com>
2025-05-03 11:32:41 +0300

    draft

---
+++ Shared/Model/Entities/Location.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.Model.Entities;
+
+public class Location
+{
+    [Key]
+    public int GeoId { get; set; }
+    public float Latitude { get; set; }
+    public float Longitude { get; set; }
+    public string Name { get; set; }
+    public string CountryCode { get; set; }
+}
---
+++ Shared/Model/Entities/Tour.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.Model.Entities;
+
+public class Tour
+{
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public TimeSpan Duration { get; set; }
+}
